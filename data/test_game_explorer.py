e81eed975478a2d41f3a655db1247a246e1e4fea
DeepQZero <deepqzero@gmail.com>
2020-12-13 22:57:38 -0600

    Formatted pictures for plays

--- data/test_game_explorer.py
+++ data/test_game_explorer.py
@@ -3,15 +3,27 @@ import datetime
 import pickle
 
 
+# game_data = pickle.load(open(r'game_data\game2018090600.p', 'rb'))
+# print(game_data.keys())
+# plays = game_data['plays']
+# for key in plays.keys():
+#     stamps = plays[key]['stamps']
+#     num_stamps = len(stamps)
+#     players = set()
+#     for stamp in stamps:
+#         players.add(stamp[9])
+#     num_players = len(players)
+#     print(num_stamps, num_players, num_stamps / num_players)
+
+
 game_data = pickle.load(open(r'game_data\game2018090600.p', 'rb'))
 print(game_data.keys())
 plays = game_data['plays']
 for key in plays.keys():
+    print(plays[key].keys())
     stamps = plays[key]['stamps']
-    num_stamps = len(stamps)
-    players = set()
+    all_stamps = []
     for stamp in stamps:
-        players.add(stamp[9])
-    num_players = len(players)
-    print(num_stamps, num_players, num_stamps / num_players)
-
+        all_stamps.append((float(stamp[0]), float(stamp[1]), float(stamp[2])))
+    all_stamps.sort()
+    # print(all_stamps)
